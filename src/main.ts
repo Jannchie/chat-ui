4aa1ed7d5c39cd8034101be623016865bbcad5bb
Jannchie <jannchie@gmail.com>
2025-08-04 01:44:07 +0900

    refactor(database)!: remove database reset utility

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router/router'
-import './composables/useDatabaseReset'
-
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 
